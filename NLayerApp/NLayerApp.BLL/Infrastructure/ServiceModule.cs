6070e3a41870db2b5b6ab9a3ddd1802da6c91bc1
Artyom07112 <37908732+Artyom07112@users.noreply.github.com>
2021-09-19 20:44:08 +0300

    database

---
+++ NLayerApp/NLayerApp.BLL/Infrastructure/ServiceModule.cs
@@ -0,0 +1,24 @@
+﻿using Ninject.Modules;
+using Nlayeer.DAL.Repositories;
+using NLayerApp.DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NLayerApp.BLL.Infrastructure
+{
+    public class ServiceModule : NinjectModule
+    {
+        private string connectionString;
+        public ServiceModule(string connection)
+        {
+            connectionString = connection;
+        }
+        public override void Load()
+        {
+            Bind<IUnitOfWork>().To<EFUnitOfWork>().WithConstructorArgument(connectionString);
+        }
+    }
+}
---
+++ NLayerApp/NLayerApp.WEB/Util/OrderModule.cs
@@ -0,0 +1,18 @@
+﻿using Ninject.Modules;
+using Nlayer.BLL.Service;
+using NLayerApp.BLL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace NLayerApp.WEB.Util
+{
+    public class OrderModule : NinjectModule
+    {
+        public override void Load()
+        {
+            Bind<IOrderService>().To<OrderService>();
+        }
+    }
+}
